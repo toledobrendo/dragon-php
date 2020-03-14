915aaf8d0397824ab20ec63bd82e5d66483e0b6e
MeisterVins <allain55x@gmail.com>
2020-03-14 14:33:39 +0800

    Hey Ho (2)

---
+++ bobs-auto-parts/process-order.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</head>
+<body>
+	<div class="container">
+		<div class="card">
+			<div class="card-body">
+				<h3 class="card-title">Order Result</h3>
+				<?php
+					echo '<p>Order Processed at ';
+					echo date('H:i, jS F Y');
+					echo '</p>';
+
+					/**
+						Uy nice!
+						Pruuuuu	
+					**/
+
+					$tireQty = $_POST['tireQty'];
+					$oilQty = $_POST['oilQty'];
+					$sparkQty = $_POST['sparkQty'];
+
+					echo '<p>Your order is as follows</p>';
+					echo $tireQty.' tire/s<br/>';
+					echo $oilQty.' bottle/s of oil<br/>';
+					echo $sparkQty.' spark plug/s<br/>';
+
+				?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
