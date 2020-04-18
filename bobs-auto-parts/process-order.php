85fbc20c56cde77ae7509ad39e13c36be782f380
Francis Fabul <37649179+StaticalTech90@users.noreply.github.com>
2020-04-18 15:08:15 +0800

    added Go Back Button

--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -43,6 +43,9 @@
                          */
                     ?>
             </div>
+            <div class="card-footer">
+                <a class="btn btn-info" href="order-form.php">Go Back</a>
+            </div>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
