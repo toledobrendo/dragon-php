9cf2cf938bb3ddbaf2f708789d597be208320c89
MeisterVins <allain55x@gmail.com>
2020-04-18 16:13:34 +0800

    Adding vatable and vat

--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
+<?php
+	define('TIRE_PRICE', 100);
+	define('OIL_PRICE', 50);
+	define('SPARK_PRICE', 30);
+?>
+
 <html>
 <head>
 	<meta charset="utf-8">
@@ -26,16 +32,43 @@
 						Pruuuuu	
 					**/
 
-					$tireQty = $_POST['tireQty'];
-					$oilQty = $_POST['oilQty'];
-					$sparkQty = $_POST['sparkQty'];
+					$tireQty = $_POST['tireQty'] ? $_POST['tireQty'] : 0;
+					$oilQty = $_POST['oilQty'] ? $_POST['oilQty'] : 0;
+					$sparkQty = $_POST['sparkQty'] ? $_POST['sparkQty'] : 0;
 
 					echo '<p>Your order is as follows</p>';
 					echo $tireQty.' tire/s<br/>';
 					echo $oilQty.' bottle/s of oil<br/>';
-					echo $sparkQty.' spark plug/s<br/>';
+					echo $sparkQty.' spark plug/s<br/><br/>';
 
+					/**
+					echo '<p>Prices<br/>';
+					echo 'Tires: '.TIRE_PRICE.'<br/>';
+					echo 'Oil: '.OIL_PRICE.'<br/>';
+					echo 'Spark Plug: '.SPARK_PRICE.'<br/><br/>';
+
+					$totalQty = @($tireQty + $oilQty + $sparkQty);
+					echo 'Total Quantity: '.$totalQty.'<br/>';
+					**/
+
+					$tireAmount = @($tireQty * TIRE_PRICE);
+					$oilAmount = @($oilQty * OIL_PRICE);
+					$sparkAmount = @($sparkQty * SPARK_PRICE);
+
+					$totalAmount = (float) ($tireAmount + $oilAmount + $sparkAmount);
+					echo 'Total Amount: ' .$totalAmount.'<br/>';
+
+					$vat = (float) (0.12 * $totalAmount);
+					echo "VAT Amount: " .$vat.'<br/>';
+
+					$vatableAmount = (float) ($totalAmount - $vat);
+					echo "VATable Amount: " .$vatableAmount.'<br/><br/>';
+
+					echo 'Amount Exceeded 500 but less than 1000?'.($totalAmount > 500 && $totalAmount < 1000 ? ' Yes' : ' No').'<br/>';
 				?>
+			</div>
+			<div class="card-footer">
+				<a class="btn btn-info" href="order-form.php">Go Back</a>
 			</div>
 		</div>
 	</div>
