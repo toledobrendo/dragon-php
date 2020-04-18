858c968e9f65e68e51e006c2689dc08e054da94e
BuildTools <unconfigured@null.spigotmc.org>
2020-04-18 16:29:10 +0800

    A3_Tax

--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -1,7 +1,14 @@
+<?php
+  define('TIRE_PRICE', 100);
+  define('OIL_PRICE', 100);
+  define('SPARK_PRICE', 100);
+  define('VAT_RATE', .12);
+  define('VATABLE_RATE', 1.12);
+?>
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>Resulta ni Bob</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,21 +23,30 @@
                 <h2>Bob's Auto Parts</h2>
                 <h3 class="card-title">Order Result</h3>
                 <?php
+                    /* Get Data */
                     $tireQty = $_POST['tireQty'];
                     $oilQty = $_POST['oilQty'];
                     $sparkQty = $_POST['sparkQty'];
-
+                    /* Process Data */
+                    $totalPrice = (float)($tireQty*TIRE_PRICE)+($oilQty*OIL_PRICE)+($sparkQty*SPARK_PRICE);
+                    $vatAble = (float)$totalPrice / VATABLE_RATE;
+                    $vatAmnt = (float)VAT_RATE*($totalPrice/VATABLE_RATE);
+                    /* Print */
                     echo "<p>Order Processed at ";
                     echo date("H:i, jS F Y");
                     echo "</p>";
                     echo "Your order is as follows<br><br>";
                     echo "$tireQty tires.<br>";
                     echo "$oilQty bottles of oil.<br>";
-                    echo "$sparkQty spark plugs.<br>";
+                    echo "$sparkQty spark plugs.<br><br>";
+                    echo "Vatable Amount: $vatAble<br>";
+                    echo "VAT Amount (12%): $vatAmnt<br>";
+                    echo 'Total Amount: ' .$totalPrice.'<br/><br/>';
+                    echo 'Total Price exceeded 500 but less than 1000? '.($totalPrice > 500 && $totalPrice < 1000? 'Yes' : 'No').'<br/>';
                 ?>
             </div>
             <div class="card-footer">
-              <a class="btn btn-info" href="order-form.php"></a>
+              <a class="btn btn-info" href="order-form.php">Back to home</a>
             </div>
         </div>
     </div>
