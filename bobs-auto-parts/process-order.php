f43a6cbaa58a0fe6762c4d135ca82e943277cdcd
DePLeTe <tiongco.daniel158@gmail.com>
2020-04-18 17:47:49 +0800

    loops and freight cost added

--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -34,17 +34,48 @@
 					$sparkQty = $_POST['sparkQty'] ? $_POST['sparkQty'] : 0;
 					$totalQty = @($tireQty + $oilQty + $sparkQty);
 
-					echo '<p>Your order is as follows</p>';
-					echo "$tireQty tires<br/>";
-					echo "$oilQty oil<br/>";
-					echo "$sparkQty sparks<br/><br/>";
-					//different way of display
-					//echo $tireQty.' $tireQty tires<br/>';
+					$find = $_POST['find'];
+
+					switch ($find) {
+						case 'regular':
+							echo 'Regular Customer';
+							break;
+
+						case 'tv':
+							echo 'From TV Advertising';
+							break;
+
+						case 'phone':
+							echo 'From Phone Directory';
+							break;
+
+						case 'mouth':
+							echo 'From word of mouth';
+							break;
+						
+						default:
+							echo 'Unknown Customer';
+							break;
+					}
 
 					echo '<p>Prices<br/>';
 					echo 'Tires: '.TIRE_PRICE.'<br/>';
 					echo 'Oil: '.OIL_PRICE.'<br/>';
-					echo 'Sparks: '.SPARK_PRICE.'<br/>';
+					echo 'Sparks Plugs: '.SPARK_PRICE.'<br/><br/>';
+
+					if($totalQty == 0){
+						echo 'You did not order anything! <br/><br/>';
+					} else {
+						echo '<p>Your order is as follows</p>';
+						if($tireQty > 0)
+							echo "$tireQty tires<br/>";
+						if($oilQty > 0)
+							echo "$oilQty oil<br/>";
+						if($sparkQty > 0)
+							echo "$sparkQty spark plugs<br/>";
+						//different way of display
+						//echo $tireQty.' $tireQty tires<br/>';
+					}
 
 					$tireAmount = @($tireQty * TIRE_PRICE);
 					$oilAmount = @($oilQty * OIL_PRICE);
@@ -56,14 +87,23 @@
 					$vatableAmount = @((float)($totalAmount / 1.12));
 					$vatAmount = @((float)($vatableAmount * 0.12));
 					
-
-					echo '<br/>';
 					echo 'Total Quantity: '.$totalQty.'<br/>';
 					echo 'VATABLE Amount: '.$vatableAmount.'<br/>';
 					echo 'VAT Amount(12%): '.$vatAmount.'<br/>';
 					echo 'Total Amount: '.$totalAmount.'<br/>';
 					echo 'Other Total Amount: '.$otherTotalAmount.'<br/>';
-					echo 'Amount Exceeded 500 but less than 1000? '.($totalAmount > 500 && $totalAmount < 1000 ? 'Yes' : 'No').'<br/>';
+					echo 'Amount Exceeded 500 but less than 1000? '.($totalAmount > 500 && $totalAmount < 1000 ? 'Yes' : 'No').'<br/><br/>';
+
+					echo 'Is $totalAmount String? '.(is_string($totalAmount) ? 'Yes' : 'No').'<br/>';
+
+					unset($totalAmount);
+					//checks if the variable has a value set to it
+					echo 'Is $totalAmount set? '.(isset($totalAmount) ? 'Yes' : 'No').'<br/>';
+
+					$totalAmountTwo = "";
+					echo 'Is $totalAmountTwo set? '.(isset($totalAmountTwo) ? 'Yes' : 'No').'<br/>';
+					//if 0 or "" it will show 'Yes'
+					echo 'Is $totalAmountTwo empty? '.(empty($totalAmountTwo) ? 'Yes' : 'No').'<br/>';
 				?>
 				<div class="card-footer">
 					<a class="btn btn-info float-right" href="order-form.php">Go Back</a>
