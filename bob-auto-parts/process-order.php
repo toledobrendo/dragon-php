b02077365c543635c9384ca13be7bab65c113a28
Mel Ivan Magsino <konlover999@gmail.com>
2020-04-18 16:32:37 +0800

    test

--- bob-auto-parts/process-order.php
+++ bob-auto-parts/process-order.php
@@ -1,4 +1,10 @@
 <!doctype html>
+<?php 
+    // constant variables
+    define('TIRE_PRICE', 100);
+    define('OIL_PRICE', 50);
+    define('SPARK_PRICE', 20);
+?>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -21,22 +27,52 @@
             echo date ('H:i,jS F Y');
             echo '</p>';
 
-            $tireQty= $_POST['tireQty'];
-            $oilQty= $_POST['oilQty'];
-            $sparkQty= $_POST['sparkQty'];
+            $tireQty= $_POST['tireQty']? $_POST['tireQty'] : 0;
+            $oilQty= $_POST['oilQty']? $_POST['oilQty'] : 0;
+            $sparkQty= $_POST['sparkQty']? $_POST['sparkQty'] : 0;
 
             echo '<p> Your order is as follows</p>';
             echo $tireQty.'tires<br/>';
-            echo "$tireQty tires <br/>";
 
             
             echo $oilQty.'bottles of oil<br/>';
 
             
-            echo $sparkQty.'Spark Plugs<br/>';
+            echo $sparkQty.'Spark Plugs<br/><br/>';
 
+            echo '<p>Prices<br/>';
+            echo 'Tires:'.TIRE_PRICE.'<br/>';
+            echo 'Oil:'.OIL_PRICE.'<br/>';
+            echo 'Spark Plugs: '.SPARK_PRICE.'<br/><br/>';
+
+            $totalQty=@($tireQty+$oilQty+$sparkQty);
+            echo 'Total Quantity: '.$totalQty.'<br/>';
+
+            $tireAmount = @($tireQty * TIRE_PRICE);
+            $oilAmount = @($oilQty * OIL_PRICE);
+            $sparkAmount = @($sparkQty * SPARK_PRICE);
+            
+            $totalAmount=(float) $tireAmount;
+
+            $otherTotalAmount = &$totalAmount;
+            $otherTotalAmount += $oilAmount;
+            $totalAmount += $sparkAmount;
+
+            $VAT = $totalAmount * 0.12;
+            $VATable = $totalAmount - $VAT;
+            $VATAmount = 0.12 * $VATable;
+            $Total = $VATAmount + $VATable;
+
+            echo 'Other Total Amount: '.$otherTotalAmount.'<br/>';
+            echo 'Total Amount:'.$totalAmount.'<br/>';
+            echo 'Amount exceeded 500?  '.($totalAmount > 500 ? 'Yes' :'No').'<br/>';
+            echo 'VATable Amount: '.($Total).'<br/>';
+            echo 'VAT Amount: '.($VATAmount).'<br/>';
           ?>
   			</div>
+        <div class="card-footer">
+          <a class="btn btn-info" href="order-form.php">Go Back</a>
+        </div>
   		</div>
   	</div>
  
