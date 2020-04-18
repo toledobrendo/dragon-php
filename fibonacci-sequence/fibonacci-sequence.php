a80cd0872883681cec558ef9794d997f9ae13ae6
Mad-Rack <fusionmadrack@gmail.com>
2020-04-18 20:05:52 +0800

    fibonacci

---
+++ fibonacci-sequence/fibonacci-sequence.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="card">
+				<div class="card-body">
+					<h1>Fibonacci Sequence</h1>
+					<form method="POST">
+						<table class="table">
+							<thead>
+								<tr class="row">
+									<td><input type="number" name="sequence-length" min="1"></td>
+									<td><button type="submit" class="btn btn-primary">Submit</button></td>
+								</tr>
+							</thead>
+						</table>
+					</form>
+
+					<table class="table">
+						<tbody>
+							<tr class="row">
+								<?php 
+									if(isset($_POST['sequence-length'])) {
+										$current = 0;
+										$prev = -1;
+										$temp = 0;
+										$length = $_POST['sequence-length'];
+
+										echo 'Sequence length: '.$length;
+
+										for($counter = 0; $counter < $length; $counter++) {
+											if($current == 0) {
+												if($prev != 0) {
+													echo '<td class="col-1">'.$current.'</td>';
+													$prev += 1;
+												} else {
+													echo '<td class="col-1">'.$current.'</td>';
+													$current += 1;
+												}
+												
+											} else {
+												$temp = $current;
+												echo '<td class="col-1">'.($current += $prev).'</td>';
+												$prev = $temp;
+											}
+											
+										}
+									}
+								 ?>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+
+	    <!-- Optional JavaScript -->
+	    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</body>
+</html>
