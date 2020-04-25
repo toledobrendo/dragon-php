2927aa035e16b01d35159a6e151f9f3f97c29351
IamJayce <johncarlo.jumawan212@gmail.com>
2020-04-25 17:03:11 +0800

    Commit

--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -30,20 +30,47 @@
             $tireQty = $_POST['tireQty'] ? $_POST['tireQty'] : 0;
             $oilQty = $_POST['oilQty'] ? $_POST['oilQty'] : 0;
             $sparkQty = $_POST['sparkQty'] ? $_POST['sparkQty'] : 0;
+            $find = $_POST['find'];
 
-            
-            echo '<p>Your order is as follows</p>';
-            // echo $tireQty.' $tireQty tires<br/>';
-            echo "$tireQty tires<br/>";
-            echo $oilQty.' oil<br/>';
-            echo $sparkQty.' spark plugs<br/><br/>';
+            switch($find) {
+              case 'regular':
+                echo 'Regular Customer';
+                break;
+              case 'tv':
+                echo 'From TV Advertising';
+                break;
+              case 'phone':
+                echo 'From Phone Directory';
+                break;
+              case 'mouth':
+                echo 'From Word of Mouth';
+                break;
+              default:
+                echo 'Unknown Customer';
+                break;
+            }
 
+            echo '<br/><br/>';
             echo '<p>Prices<br/>';
             echo 'Tires: '.TIRE_PRICE.'<br/>';
             echo 'Oil: '.OIL_PRICE.'<br/>';
             echo 'Spark Plugs: '.SPARK_PRICE.'<br/><br/>';
 
             $totalQty = @($tireQty + $oilQty + $sparkQty);
+
+            if ($totalQty == 0) {
+              echo 'You didn\'t order anything. <br/> <br/>';
+            } else {
+              echo '<p>Your order is as follows</p>';
+              // echo $tireQty.' $tireQty tires<br/>';
+              if ($tireQty > 0)
+                echo "$tireQty tires<br/>";
+              if ($oilQty > 0)
+                echo $oilQty.' oil<br/>';
+              if ($sparkQty > 0)
+                echo $sparkQty.' spark plugs<br/>';
+              echo '<br/>';
+            }
             echo 'Total Quantity: '.$totalQty.'<br/>';
 
             $tireAmount = @($tireQty * TIRE_PRICE);
@@ -59,17 +86,23 @@
             echo 'Other Total Amount: '.$otherTotalAmount.'<br/>';
             echo 'Total Amount: '.$totalAmount.'<br/>';
 
-            echo 'Amount exceeded 500? '.($totalAmount > 500 ? 'Yes' : 'No').'<br/>';
+            $vatableAmount = $totalAmount / 1.12;
+            $vat = $totalAmount - $vatableAmount;
 
-            $VATable  = $totalAmount / 1.12;
-            $VAT = $VATable * 0.12;
-            $totalAmountVatComputation = $VAT + $VATable;
+            echo 'VATable Amount: '.$vatableAmount.'<br/>';
+            echo 'VAT: '.$vat.'<br/>';
 
-            echo 'VATable Amount: '.$VATable.'<br/>';
-            echo 'VAT (12%): '.$VAT.'<br/>';
-            echo 'Total Amount: '.$totalAmountVatComputation.'<br/>';
+            echo 'Amount exceeded 500? '.($totalAmount > 500 ? 'Yes' : 'No').'<br/><br/>';
 
+            echo 'Is $totalAmount string? '.(is_string($totalAmount) ? 'Yes' : 'No').'<br/>';
 
+            unset($totalAmount);
+
+            echo 'Is $totalAmount set? '.(isset($totalAmount) ? 'Yes' : 'No').'<br/>';
+
+            $totalAmountTwo = 0;
+            echo 'Is $totalAmountTwo set? '.(isset($totalAmountTwo) ? 'Yes' : 'No').'<br/>';
+            echo 'Is $totalAmountTwo empty? '.(empty($totalAmountTwo) ? 'Yes' : 'No').'<br/>';
           ?>
         </div>
         <div class="card-footer">
