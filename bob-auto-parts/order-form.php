98e45dd496ad1b2c4dde676c528724ad19242d4a
Ivan Magsino <201801483@iacademy.edu.ph>
2020-04-25 17:21:56 +0800

    updated order-form and price-list

--- bob-auto-parts/order-form.php
+++ bob-auto-parts/order-form.php
@@ -18,14 +18,41 @@
             <h3 class="card-title"> ORDERFORM</h3>
             <form action="process-order.php" method="post">
               <table class="table">
-                <thead>
+<!--                 <thead>
                   <tr class="row">
                     <th class="col-5">Item</th>
                     <th class="col-4">Quantity</th>
                   </tr>
-                </thead>
+                </thead> -->
                 <tbody>
-                  <tr class="row">
+                  <?php
+                    $items = array(
+                        array('itemName' => 'Oil','name'=>'tireQty','price'=>100),
+                        array('itemName' => 'Tires','name'=>'oilQty','price'=>200),
+                        array('itemName' => 'Spark Plugs','name'=>'sparkQty','price'=>300)
+                      );
+
+                     echo '<table class="table table-condensed">
+                            <thead>
+                            <tr>
+                              <th>Item</th>
+                              <th>Quantity</th>
+                              <th>Price</th>
+                            </tr>
+                            </thead>';
+                    foreach ($items as $item) {
+                        echo '<tr>';
+                          echo '<td class="col-2">'.$item['itemName'].'</td>';
+                          echo '<td class="col-5">
+                            <input type="number" name="'.$item['name'].'" maxlength="3" min="0" max="10" class="form-control" />
+                          </td>';
+                          echo '<td class="col-2">'.$item['price'].'</td>';
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+                  ?>
+
+<!--                   <tr class="row">
                     <td class="col-5">Tires</td>
                     <td class="col-4">
                       <input type="number" name="tireQty" maxlength="3" min="0" max="10" class="form-control" />
@@ -42,10 +69,11 @@
                     <td class="col-4">
                       <input type="number" name="sparkQty" maxlength="3" min="0" max="10" class="form-control" />
                     </td>
-                  </tr>
+                  </tr> -->
+
                   <tr class="row">
                       <td class="col-5">How did you find Bob's</td>
-                      <td class="col-4">
+                      <td class="col-5">
                         <select name="find" class="customer-select">
                           <option value="regular">I'm a regular customer</option>
                           <option value="tv">TV advertising</option>
--- bob-auto-parts/price-list.php
+++ bob-auto-parts/price-list.php
@@ -20,39 +20,104 @@
 
             echo '</p>Product 0: '.$products[0];
 
+            sort($products);
+            rsort($products);
+
             echo '<ul>';
-            for($ctr = 0; $ctr < count($products); $ctr++){
+            for ($ctr = 0; $ctr < count($products); $ctr++) {
               echo '<li>'.$products[$ctr].'</li>';
             }
             echo '</ul>';
 
             echo '<ul>';
-            int $ctr = 0;
-              foreach ($products as &$product) {
-                $product = $product.' -1 ';
-                echo '<li>'.ctr. ' - '.product.'</li>';
-                $ctr++;
-              }
+            $ctr = 0;
+            foreach ($products as &$product) {
+              $product = $product.' - 1';
+              echo '<li>'.$ctr.' - '.$product.'</li>';
+              $ctr++;
+            }
             echo '</ul>';
 
             echo '<ul>';
-            for($ctr = 0; $ctr < count($products); $ctr++){
+            for ($ctr = 0; $ctr < count($products); $ctr++) {
               echo '<li>'.$products[$ctr].'</li>';
             }
             echo '</ul>';
 
             $numbers = range(1, 10, 2);
-            echo '<br/> range (1,10): ';
+            echo '<br/>range(1, 10): ';
             foreach ($numbers as $number) {
-              echo $number.' ';
+              echo $number.', ';
             }
             echo '<br/>';
 
-            $letters = range('a','z');
-            echo '<br/> letters: ';
+            $letters = range('a', 'z');
+            echo '<br/>letters: ';
             foreach ($letters as $letter) {
               echo $letter.' ';
             }
+            echo '<br/>';
+
+            $prices = array('Tires' => 100, 'Oil' => 20, 'Spark Plugs' => 5, 1000);
+
+            $prices['Tires'] = 120;
+            echo 'Tire price: '.$prices['Tires'];
+
+            echo '<br/>Fourth price: '.$prices[0];
+
+            $prices['Clutch Disk'] = 250;
+
+            echo '<br/>Clutch Disk: '.$prices['Clutch Disk'];
+
+            ksort($prices);
+            krsort($prices);
+            asort($prices);
+            arsort($prices);
+
+            echo '<ul>';
+            foreach($prices as $itemDesc => $price) {
+              echo '<li>'.$itemDesc.' - '.$price.'</li>';
+            }
+            echo '</ul>';
+
+            $empty = array();
+
+            $items = array(
+                        array('Code' => 'OIL', 'Description' => 'Oil', 'Price' => 10),
+                        array('Code' => 'TIR', 'Description' => 'Tires', 'Price' => 100),
+                        array('Code' => 'SPK', 'Description' => 'Spark Plugs', 'Price' => 5)
+                      );
+
+            echo '<p>'.$items[1]['Description'];
+
+            function compareItems($fir, $sec) {
+              if ($fir['Price'] == $sec['Price']) {
+                return 0;
+              } else if ($fir['Price'] > $sec['Price']) {
+                return -1;
+              } else {
+                return 1;
+              }
+            }
+
+            usort($items, 'compareItems');
+
+            echo '<table class="table table-condensed">
+                    <thead>
+                    <tr>
+                      <th>Code</th>
+                      <th>Description</th>
+                      <th>Price</th>
+                    </tr>
+                    </thead>';
+            foreach ($items as $item) {
+              echo '<tr>';
+              foreach ($item as $value) {
+                echo '<td>'.$value.'</td>';
+              }
+              echo '</tr>';
+            }
+            echo '</table>';
           ?>
         </div>
       </div>
