cb43ecfbb558ccf4bf59654b0e658d455f3f3f7c
Tian <cbgcharmander@gmail.com>
2020-04-25 16:48:10 +0800

    activity four

--- bobs-auto-parts/OrderForm.php
+++ bobs-auto-parts/OrderForm.php
@@ -18,9 +18,9 @@
 
         <?php
         $items = array(
-            array('Description'=> 'OIL', 'Price' => '<input type="number" name="tireQty" maxlength="3" min="0" max="10" class="form-control"/>'),
-            array('Description'=> 'Tires', 'Price' =>'<input type="number" name="oilQty" maxlength="3" min="0" max="10" class="form-control"/>'),
-            array('Description'=> 'Spark Plugs', 'Price' =>'<input type="number" name="spQty" maxlength="3" min="0" max="10" class="form-control"/>')
+            array('Description'=> 'OIL', 'Price' =>10, 'Input' => '<input type="number" name="tireQty" maxlength="3" min="0" max="10" class="form-control"/>'),
+            array('Description'=> 'Tires', 'Price' =>100, 'Input' =>'<input type="number" name="oilQty" maxlength="3" min="0" max="10" class="form-control"/>'),
+            array('Description'=> 'Spark Plugs', 'Price' => 1000, 'Input' =>'<input type="number" name="spQty" maxlength="3" min="0" max="10" class="form-control"/>')
           );
 
           function compareItems($fir,$sec){
@@ -34,13 +34,14 @@
           }
 
           usort($items,'compareItems');
-        
+
 
         echo '<form action="process-order.php" method="post">
               <table class="table table-condensed">
               <thead>
               <tr>
                 <th>Item</th>
+                <th>Price</th>
                 <th>Quantity</th>
               </tr>
               </thead>';
@@ -51,9 +52,9 @@
           }
           echo '</tr>';
         }
-        echo '<tr class="row">
-              <td class="col-8">How did you find Bobs</td>
-              <td class="col-4">
+        echo '<tr>
+              <td colspan="2">How did you find Bobs</td>
+              <td>
                 <select name="find" class="custom-select">
                   <option value="regular">Im a regular customer</option>
                   <option value="tv">TV advertising</option>
@@ -62,10 +63,12 @@
                 </select>
               </td>
               </tr>
-              <tr class="row">
-                <td colspan="2" class="col-12">
-                  <a href="freight-cost.php" class="btn btn-secondary float-right" style="margin-left:50px;">Freight Cost</a>
-                  <button type="submit" class="btn btn-primary float-right">Submit</button>
+              <tr>
+                <td colspan = "2">
+                  <a href="freight-cost.php" class="btn btn-secondary float-left" >Freight Cost</a>
+                </td>
+                <td>
+                <button type="submit" class="btn btn-primary float-right">Submit</button>
                 </td>
               </tr>
               </table>';
