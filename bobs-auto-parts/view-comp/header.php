2918b2faf9ce372f05efe4f5c08ec1108377b4ac
Francis Fabul <37649179+StaticalTech90@users.noreply.github.com>
2020-05-02 16:13:51 +0800

    added Navbar with active tracking; modified Footer

--- bobs-auto-parts/view-comp/header.php
+++ bobs-auto-parts/view-comp/header.php
@@ -4,10 +4,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Order Form</title>
+    <title>Bob's Auto Parts</title>
 </head>
 
 <body>
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div href="../../index.php" class="navbar-brand">Bob's Auto Parts</div>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item <?php if (strpos($_SERVER['REQUEST_URI'], 'order-form.php')) echo 'active'; ?>">
+                    <a href="order-form.php" class="nav-link">Order Form</a>
+                </li>
+                <li class="nav-item <?php if (strpos($_SERVER['REQUEST_URI'], 'freight-cost.php')) echo 'active'; ?>">
+                    <a href="freight-cost.php" class="nav-link">Freight Cost</a>
+                </li>
+                <li class="nav-item <?php if (strpos($_SERVER['REQUEST_URI'], 'price-list.php')) echo 'active'; ?>">
+                    <a href="price-list.php" class="nav-link">Price List</a>
+                </li>
+            </ul>
+        </div>
+    </div>
     <div class="container">
         <div class="card">
             <div class="card-body">
