31841defe9590436fdcef693bd47b134ffc7fc41
Tian <cbgcharmander@gmail.com>
2020-05-02 17:51:45 +0800

    lesson for today

--- iac-consulting/view-comp/header.php
+++ iac-consulting/view-comp/header.php
@@ -1,32 +1,31 @@
+<?php
+  function isActive($page) {
+    return strpos($_SERVER['REQUEST_URI'], $page);
+  }
+?>
 <html>
-<head>
-    <!-- Required meta tags -->
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-    crossorigin="anonymous">
-
-</head>
-<body>
-  <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div href="index.php" class="navbar-brand">Iac Consulting</div>
-    <div class="collapse navbar-collapse">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact</a>
-        </li>
-      </ul>
+    <link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div href="index.php" class="navbar-brand">iAC Consulting</div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item <?php if(isActive('index.php')) echo 'active'; ?>">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item <?php if(isActive('contact.php')) echo 'active'; ?>">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-  <div class="container">
-    <div class="card">
-      <div class="card-body">
+    <div class="container">
+      <div class="card">
+        <div class="card-body">
