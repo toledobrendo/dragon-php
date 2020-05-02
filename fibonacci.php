c36c9cbe614d7dc9dd4117e292e94ebff7bda6db
Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>
2020-05-02 14:04:19 +0800

    Updated title of fibonacci.php

--- fibonacci.php
+++ fibonacci.php
@@ -6,7 +6,7 @@
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
       crossorigin="anonymous">
-    <title>Template</title>
+    <title>Fibonacci Sequence</title>
   </head>
   <body>
     <div class="container">
