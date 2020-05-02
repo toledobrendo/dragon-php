7b72bfc63dafda1661bb5668fda373ec9df74fb1
CharAzu <markchristianyapan@yahoo.com>
2020-05-02 15:31:26 +0800

    added view-universal and saved iac-consulting

--- iac-consulting/index.php
+++ iac-consulting/index.php
@@ -1,17 +1,9 @@
 <?php
   require_once 'script.php';
   require_once 'message.php';
+  require_once 'view-comp/header.php';
  ?>
-<html>
 
-<head>
-	<title> Hello World </title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-
-<body>
 
 	<div class="container">
 		<div class ="card">
