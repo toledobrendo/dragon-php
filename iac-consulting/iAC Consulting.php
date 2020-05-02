47485e1f9d87871c844a70da2bd3501d6eb18291
Ivan Magsino <201801483@iacademy.edu.ph>
2020-05-02 15:46:07 +0800

    added iAC Consulting

---
+++ iac-consulting/iAC Consulting.php
@@ -0,0 +1,19 @@
+
+  <?php
+    // include('message.php');
+    require_once('script.php');
+    require_once('message.php');
+    require_once('view-comp/header.php');
+
+  ?>
+
+  <?php 
+  	echo $message;
+  ?>
+  
+  <?php 
+    require_once('view-comp/footer.php');
+  ?>
+
+  </body>
+</html>
