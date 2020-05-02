c69be05170b78f04b652a2213648fc77a83d1a71
BuildTools <unconfigured@null.spigotmc.org>
2020-05-02 15:48:06 +0800

    Did A5-REQUIRED_ONCE Footer and Header for Bob

--- bobs-auto-parts/order-form.php
+++ bobs-auto-parts/order-form.php
@@ -1,87 +1,61 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>Bob's Order Form</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-
-<body>
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <h3 class="card-title">Order Form</h3>
-                <form action="process-order.php" method="post">
-                    <table class="table">
-                        <thead>
-                            <tr class="row">
-                                <th class="col-5">Item</th>
-                                <th class="col-3">Price</th>
-                                <th class="col-4">Quantity</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- Do things -->
-                            <?php
-                            $products = array(
-                                array(
-                                    'Code' => 'tire',
-                                    'Description' => 'Tires',
-                                    'Price' => 10
-                                ),
-                                array(
-                                    'Code' => 'oil',
-                                    'Description' => 'Oil',
-                                    'Price' => 10
-                                ),
-                                array(
-                                    'Code' => 'spark',
-                                    'Description' => 'Spark Plugs',
-                                    'Price' => 10
-                                )
-                            );
-                            foreach ($products as $product) {
-                                echo '<tr class="row">';
-                                echo '<td class="col-5">' . $product['Description'] . '</td>';
-                                echo '<td class="col-3">' . $product['Price'] . '</td>';
-                                echo '<td class="col-4"><input type="number" name="' . $product['Code'] . 'Qty" maxlength="3" min="0" max="10" class="form-control"></td>';
-                                echo '</tr>';
-                            }
-                            ?>
-                            <tr class="row">
-								<td class="col-5">How did you find Bob's?</td>
-								<td class="col-7">
-									<select name="find" class="custom-select">
-										<option value="regular">I'm a regular customer</option>
-										<option value="tv">TV Advertising</option>
-										<option value="phone">Phone Directory</option>
-										<option value="mouth">Word of mouth</option>
-									</select>
-								</td>
-							</tr>
-                            <tr class="row">
-                                <td colspan="2" class="col-9">
-                                    <a href="../index.php" class="btn btn-danger float-right">Go Back</a>
-                                    <a href="freight-cost.php" class="btn btn-secondary float-right">Freight Cost</a>
-                                    <button type="submit" class="btn btn-primary float-right">Submit</button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </form>
-            </div>
-        </div>
-    </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php require_once('dependency/header.php'); ?>
+<h3 class="card-title">Order Form</h3>
+<form action="process-order.php" method="post">
+    <table class="table">
+        <thead>
+            <tr class="row">
+                <th class="col-5">Item</th>
+                <th class="col-3">Price</th>
+                <th class="col-4">Quantity</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Do things -->
+            <?php
+            $products = array(
+                array(
+                    'Code' => 'tire',
+                    'Description' => 'Tires',
+                    'Price' => 10
+                ),
+                array(
+                    'Code' => 'oil',
+                    'Description' => 'Oil',
+                    'Price' => 10
+                ),
+                array(
+                    'Code' => 'spark',
+                    'Description' => 'Spark Plugs',
+                    'Price' => 10
+                )
+            );
+            foreach ($products as $product) {
+                echo '<tr class="row">';
+                echo '<td class="col-5">' . $product['Description'] . '</td>';
+                echo '<td class="col-3">' . $product['Price'] . '</td>';
+                echo '<td class="col-4"><input type="number" name="' . $product['Code'] . 'Qty" maxlength="3" min="0" max="10" class="form-control"></td>';
+                echo '</tr>';
+            }
+            ?>
+            <tr class="row">
+                <td class="col-5">How did you find Bob's?</td>
+                <td class="col-7">
+                    <select name="find" class="custom-select">
+                        <option value="regular">I'm a regular customer</option>
+                        <option value="tv">TV Advertising</option>
+                        <option value="phone">Phone Directory</option>
+                        <option value="mouth">Word of mouth</option>
+                    </select>
+                </td>
+            </tr>
+            <tr class="row">
+                <td colspan="2" class="col-9">
+                    <a href="../index.php" class="btn btn-danger float-right">Go Back</a>
+                    <a href="freight-cost.php" class="btn btn-secondary float-right">Freight Cost</a>
+                    <button type="submit" class="btn btn-primary float-right">Submit</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form>
+<?php require_once('dependency/footer.php'); ?>
--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -1,59 +1,35 @@
 <?php
-  define('TIRE_PRICE', 100);
-  define('OIL_PRICE', 100);
-  define('SPARK_PRICE', 100);
-  define('VAT_RATE', .12);
-  define('VATABLE_RATE', 1.12);
+require_once('dependency/header.php');
+define('TIRE_PRICE', 100);
+define('OIL_PRICE', 100);
+define('SPARK_PRICE', 100);
+define('VAT_RATE', .12);
+define('VATABLE_RATE', 1.12);
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Resulta ni Bob</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <h2>Bob's Auto Parts</h2>
-                <h3 class="card-title">Order Result</h3>
-                <?php
-                    /* Get Data */
-                    $tireQty = $_POST['tireQty'];
-                    $oilQty = $_POST['oilQty'];
-                    $sparkQty = $_POST['sparkQty'];
-                    /* Process Data */
-                    $totalPrice = @(float)($tireQty*TIRE_PRICE)+($oilQty*OIL_PRICE)+($sparkQty*SPARK_PRICE);
-                    $vatAble = @(float)$totalPrice / VATABLE_RATE;
-                    $vatAmnt = @(float)VAT_RATE*($totalPrice/VATABLE_RATE);
-                    /* Print */
-                    echo "<p>Order Processed at ";
-                    echo date("H:i, jS F Y");
-                    echo "</p>";
-                    echo "Your order is as follows<br><br>";
-                    echo "$tireQty tires.<br>";
-                    echo "$oilQty bottles of oil.<br>";
-                    echo "$sparkQty spark plugs.<br><br>";
-                    echo "Vatable Amount: $vatAble<br>";
-                    echo "VAT Amount (12%): $vatAmnt<br>";
-                    echo 'Total Amount: ' .$totalPrice.'<br/><br/>';
-                    echo 'Total Price exceeded 500 but less than 1000? '.($totalPrice > 500 && $totalPrice < 1000? 'Yes' : 'No').'<br/>';
-                ?>
-            </div>
-            <div class="card-footer">
-              <a class="btn btn-info" href="order-form.php">Back to home</a>
-            </div>
-        </div>
-    </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+<h3 class="card-title">Order Result</h3>
+<?php
+/* Get Data */
+$tireQty = $_POST['tireQty'];
+$oilQty = $_POST['oilQty'];
+$sparkQty = $_POST['sparkQty'];
+/* Process Data */
+$totalPrice = @(float) ($tireQty * TIRE_PRICE) + ($oilQty * OIL_PRICE) + ($sparkQty * SPARK_PRICE);
+$vatAble = @(float) $totalPrice / VATABLE_RATE;
+$vatAmnt = @(float) VAT_RATE * ($totalPrice / VATABLE_RATE);
+/* Print */
+echo "<p>Order Processed at ";
+echo date("H:i, jS F Y");
+echo "</p>";
+echo "Your order is as follows<br><br>";
+echo "$tireQty tires.<br>";
+echo "$oilQty bottles of oil.<br>";
+echo "$sparkQty spark plugs.<br><br>";
+echo "Vatable Amount: $vatAble<br>";
+echo "VAT Amount (12%): $vatAmnt<br>";
+echo 'Total Amount: ' . $totalPrice . '<br/><br/>';
+echo 'Total Price exceeded 500 but less than 1000? ' . ($totalPrice > 500 && $totalPrice < 1000 ? 'Yes' : 'No') . '<br/>';
+?>
+</div>
+<div class="card-footer">
+  <a class="btn btn-info" href="order-form.php">Back to home</a>
+  <?php require_once('dependency/footer.php'); ?>
