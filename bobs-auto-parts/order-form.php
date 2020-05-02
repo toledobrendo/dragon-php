04d7f1c084cfcb2d52f2d96bfaaf428d68c75b10
Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>
2020-05-02 16:27:40 +0800

    Added header and footer to bobs-auto-parts pages

--- bobs-auto-parts/order-form.php
+++ bobs-auto-parts/order-form.php
@@ -1,76 +1,53 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      crossorigin="anonymous">
-    <title>Order Form</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Order Form</h3>
-          <form action="process-order.php" method="post">
-            <table class="table">
-              <thead>
-                <tr class="row">
-                  <td class="col-4">Item</td>
-                  <td class="col-4">Price</td>
-                  <td class="col-4">Quantity</td>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                  $products = array(
-                    'tires' => array('desc' => 'Tires', 'price' => 150, 'quantity' => 0),
-                    'oil' => array('desc' => 'Oil', 'price' => 10, 'quantity' => 0),
-                    'sparkplugs' => array('desc' => 'Spark Plugs', 'price' => 5, 'quantity' => 0));
+<?php
+  $title = 'Order Form';
+  require_once 'view-comp/header.php';
+?>
+<h3 class="card-title">Order Form</h3>
+<form action="process-order.php" method="post">
+  <table class="table">
+    <thead>
+      <tr class="row">
+        <td class="col-4">Item</td>
+        <td class="col-4">Price</td>
+        <td class="col-4">Quantity</td>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        $products = array(
+          'tires' => array('desc' => 'Tires', 'price' => 150, 'quantity' => 0),
+          'oil' => array('desc' => 'Oil', 'price' => 10, 'quantity' => 0),
+          'sparkplugs' => array('desc' => 'Spark Plugs', 'price' => 5, 'quantity' => 0));
 
-                  foreach ($products as $code => $data) {
-                    echo '<tr class="row">';
-                    echo '<td class="col-4">'.$data['desc'].'</td>';
-                    echo '<td class="col-4">'.$data['price'].'</td>';
-                    echo '<td class="col-4">';
-                    echo '<input class="form-control" type="number" name="'.$code.'" min="0" value="'.$data['quantity'].'" required>';
-                    echo "</td></tr>";
-                  }
-                ?>
+        foreach ($products as $code => $data) {
+          echo '<tr class="row">';
+          echo '<td class="col-4">'.$data['desc'].'</td>';
+          echo '<td class="col-4">'.$data['price'].'</td>';
+          echo '<td class="col-4">';
+          echo '<input class="form-control" type="number" name="'.$code.'" min="0" value="'.$data['quantity'].'" required>';
+          echo "</td></tr>";
+        }
+      ?>
 
-                <tr class="row">
-                  <td class="col-8">How did you find Bob's</td>
-                  <td class="col-4">
-                    <select name="find" class="custom-select">
-                      <option value="regular">I'm a Regular Customer</option>
-                      <option value="tv">TV Advertising</option>
-                      <option value="phone">Phone Directory</option>
-                      <option value="mouth">Word of Mouth</option>
-                    </select>
-                  </td>
-                </tr>
+      <tr class="row">
+        <td class="col-8">How did you find Bob's</td>
+        <td class="col-4">
+          <select name="find" class="custom-select">
+            <option value="regular">I'm a Regular Customer</option>
+            <option value="tv">TV Advertising</option>
+            <option value="phone">Phone Directory</option>
+            <option value="mouth">Word of Mouth</option>
+          </select>
+        </td>
+      </tr>
 
-                <tr class="row">
-                  <td colspan="2" class="col-12">
-                    <a href="freight-cost.php" class="btn btn-secondary float-left">Freight Cost</a>
-                    <button type="submit" class="btn btn-primary submit float-right" name="button">Submit</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </form>
-        </div>
-      </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-      crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-      crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-      crossorigin="anonymous"></script>
-  </body>
-</html>
+      <tr class="row">
+        <td colspan="2" class="col-12">
+          <a href="freight-cost.php" class="btn btn-secondary float-left">Freight Cost</a>
+          <button type="submit" class="btn btn-primary submit float-right" name="button">Submit</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</form>
+<?php require_once 'view-comp/footer.php'; ?>
--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -1,110 +1,82 @@
 <?php
+  require_once 'view-comp/header.php';
   define('TIRE_PRICE', 150);
   define('OIL_PRICE', 10);
   define('SPARK_PRICE', 5);
 ?>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      crossorigin="anonymous">
-  </head>
-  <body>
-    <!-- hello-world.php or hello_world.php -->
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Order Result</h3>
-          <?php
-            echo '<p>Order Processed at ';
-            echo date('H:i, jS F Y');
-            echo '</p>';
 
-            // PHP Comments
-            /**Multiline Comments
-              Wow**/
+<h3 class="card-title">Order Result</h3>
+<?php
+  echo '<p>Order Processed at ';
+  echo date('H:i, jS F Y');
+  echo '</p>';
 
-            $tireQty = $_POST['tires'] ? $_POST['tires'] : 0;
-            $oilQty = $_POST['oil'] ? $_POST['oil'] : 0;
-            $sparkQty = $_POST['sparkplugs'] ? $_POST['sparkplugs'] : 0;
-            $find = $_POST['find'];
+  $tireQty = $_POST['tires'] ? $_POST['tires'] : 0;
+  $oilQty = $_POST['oil'] ? $_POST['oil'] : 0;
+  $sparkQty = $_POST['sparkplugs'] ? $_POST['sparkplugs'] : 0;
+  $find = $_POST['find'];
 
-            switch($find) {
-              case 'regular':
-                echo 'Regular Customer';
-                break;
-              case 'tv':
-                echo 'From TV Advertising';
-                break;
-              case 'phone':
-                echo 'From Phone Directory';
-                break;
-              case 'mouth':
-                echo 'From Word of Mouth';
-                break;
-              default:
-                echo 'Unknown Customer';
-                break;
-            }
+  switch($find) {
+    case 'regular':
+      echo 'Regular Customer';
+      break;
+    case 'tv':
+      echo 'From TV Advertising';
+      break;
+    case 'phone':
+      echo 'From Phone Directory';
+      break;
+    case 'mouth':
+      echo 'From Word of Mouth';
+      break;
+    default:
+      echo 'Unknown Customer';
+      break;
+  }
 
-            echo '<br/><br/>';
-            echo '<p>Prices<br/>';
-            echo 'Tires: '.TIRE_PRICE.'<br/>';
-            echo 'Oil: '.OIL_PRICE.'<br/>';
-            echo 'Spark Plugs: '.SPARK_PRICE.'<br/><br/>';
+  echo '<br/><br/>';
+  echo '<p>Prices<br/>';
+  echo 'Tires: '.TIRE_PRICE.'<br/>';
+  echo 'Oil: '.OIL_PRICE.'<br/>';
+  echo 'Spark Plugs: '.SPARK_PRICE.'<br/><br/>';
 
-            $totalQty = @($tireQty + $oilQty + $sparkQty);
+  $totalQty = @($tireQty + $oilQty + $sparkQty);
 
-            if ($totalQty == 0) {
-              echo 'You didn\'t order anything. <br/> <br/>';
-            } else {
-              echo '<p>Your order is as follows:</p>';
-              if ($tireQty > 0)
-                echo "$tireQty tires<br/>";
-              if ($oilQty > 0)
-                echo $oilQty.' oil<br/>';
-              if ($sparkQty > 0)
-                echo $sparkQty.' spark plugs<br/>';
-              echo '<br/>';
-            }
-            echo 'Total Quantity: '.$totalQty.'<br/>';
+  if ($totalQty == 0) {
+    echo 'You didn\'t order anything. <br/> <br/>';
+  } else {
+    echo '<p>Your order is as follows:</p>';
+    if ($tireQty > 0)
+      echo "$tireQty tires<br/>";
+    if ($oilQty > 0)
+      echo $oilQty.' oil<br/>';
+    if ($sparkQty > 0)
+      echo $sparkQty.' spark plugs<br/>';
+    echo '<br/>';
+  }
+  echo 'Total Quantity: '.$totalQty.'<br/>';
 
-            $tireAmount = @($tireQty * TIRE_PRICE);
-            $oilAmount = @($oilQty * OIL_PRICE);
-            $sparkAmount = @($sparkQty * SPARK_PRICE);
+  $tireAmount = @($tireQty * TIRE_PRICE);
+  $oilAmount = @($oilQty * OIL_PRICE);
+  $sparkAmount = @($sparkQty * SPARK_PRICE);
 
-            $totalAmount = (float) $tireAmount;
+  $totalAmount = (float) $tireAmount;
 
-            $otherTotalAmount = &$totalAmount;
-            $otherTotalAmount += $oilAmount;
-            $totalAmount += $sparkAmount;
+  $otherTotalAmount = &$totalAmount;
+  $otherTotalAmount += $oilAmount;
+  $totalAmount += $sparkAmount;
 
-            echo 'Other Total Amount: '.$otherTotalAmount.'<br/>';
-            echo 'Total Amount: '.$totalAmount.'<br/>';
+  echo 'Other Total Amount: '.$otherTotalAmount.'<br/>';
+  echo 'Total Amount: '.$totalAmount.'<br/>';
 
-            $vatableAmount = $totalAmount / 1.12;
-            $vat = $totalAmount - $vatableAmount;
+  $vatableAmount = $totalAmount / 1.12;
+  $vat = $totalAmount - $vatableAmount;
 
-            echo 'VATable Amount: '.$vatableAmount.'<br/>';
-            echo 'VAT: '.$vat.'<br/>';
-          ?>
-        </div>
-        <div class="card-footer">
-          <a class="btn btn-info" href="order-form.php">Go Back</a>
-        </div>
-      </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-      crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-      crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-      crossorigin="anonymous"></script>
-  </body>
-</html>
+  echo 'VATable Amount: '.$vatableAmount.'<br/>';
+  echo 'VAT: '.$vat.'<br/>';
+?>
+</div>
+<div class="card-footer">
+<a class="btn btn-info" href="order-form.php">Go Back</a>
+
+<?php require_once 'view-comp/footer.php'; ?>
