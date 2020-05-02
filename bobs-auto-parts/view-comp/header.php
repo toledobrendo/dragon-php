f47722866059b182f0e54da82910f722bb0387c1
NCIcaro <nhoriza008@gmail.com>
2020-05-02 16:12:25 +0800

    update activity

--- bobs-auto-parts/view-comp/header.php
+++ bobs-auto-parts/view-comp/header.php
@@ -25,15 +25,15 @@
                 <div class="collapse navbar-collapse">
                     <ul class="navbar-nav">
                         
-                        <li class="nav-item active">
+                        <li class="nav-item <?php if(strpos($_SERVER['REQUEST_URI'], 'order-form.php')) echo 'active'; ?>">
                             <a class="nav-link" href="order-form.php">Order Form</a>
                         </li>
                         
-                        <li class="nav-item">
+                        <li class="nav-item <?php if(strpos($_SERVER['REQUEST_URI'], 'price-list.php')) echo 'active'; ?>">
                             <a class="nav-link" href="price-list.php">Price List</a>
                         </li>
                         
-                        <li class="nav-item">
+                        <li class="nav-item <?php if(strpos($_SERVER['REQUEST_URI'], 'freight-cost.php')) echo 'active'; ?>">
                             <a class="nav-link" href="freight-cost.php">Freight Cost</a>
                         </li>
                     
