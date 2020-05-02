c945643a5c82f0ee16a86f2b3bf9f723d39d3ed2
CharAzu <markchristianyapan@yahoo.com>
2020-05-02 15:46:25 +0800

    Finished A5: Order Form Header Footer

--- bobs-auto-parts/order-form.php
+++ bobs-auto-parts/order-form.php
@@ -1,69 +1,51 @@
-<html>
+<?php
+	require_once('view/header.php');
+?>
+	<h3 class="card-title"> Order Form </h3>
 
-<head>
-	<title> Order Form </title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
+	<form action="process-order.php" method="POST">
+		<table class="table">
+			<thead>
+				<tr class="row">
+					<th class="col-4"> Item </th>
+					<th class="col-4"> Price </th>
+					<th class="col-4"> Quantity </th>
+				</tr>
+			</thead>
 
-<body>
+			<tbody>
+				<?php
+					$list = array(
+						array('item name' => 'Tires', 'input name' => 'tireQty', 'price' => 100),
+						array('item name' => 'Oil', 'input name' => 'oilQty', 'price' => 20),
+						array('item name' => 'Spark Plugs', 'input name' => 'sparkQty', 'price' => 50)
+					);
 
-	<div class="container">
-		<div class ="card">
-			<div class="card-body">
-				<h3 class="card-title"> Order Form </h3>
-
-				<form action="process-order.php" method="POST">
-					<table class="table">
-						<thead>
-							<tr class="row">
-								<th class="col-4"> Item </th>
-								<th class="col-4"> Price </th>
-								<th class="col-4"> Quantity </th>
-							</tr>
-						</thead>
-
-						<tbody>
-							<?php
-								$list = array(
-									array('item name' => 'Tires', 'input name' => 'tireQty', 'price' => 100),
-									array('item name' => 'Oil', 'input name' => 'oilQty', 'price' => 20),
-									array('item name' => 'Spark Plugs', 'input name' => 'sparkQty', 'price' => 50)
-								);
-
-								foreach ($list as $item) {
-									echo '<tr class="row">';
-									echo '<td class="col-4">'.$item['item name'].'</td>';
-									echo '<td class="col-4">'.$item['price'].'</td>';
-									echo '<td class="col-4"> <input type="number" name="'.$item['input name'].'" max="10" min="0" maxLength="3" class="form-control"> </td>';
-									echo '</tr>';
-								}
-							?>
-							<tr class="row">
-								<td class="col-8">How did you find Bob's</td>
-								<td class="col-4">
-									<select name="find" class="custom-select">
-										<option value="regular">I'm a regular customer</option>
-										<option value="tv">TV advertising</option>
-										<option value="phone">Phone Directory</option>
-										<option value="mouth">Word of mouth</option>
-									</select>
-								</td>
-							</tr>
-							<tr class="row">
-								<td colspan="2" class="col-12"> <button type="submit" class="btn btn-primary float-right" > SUBMIT </button> </td>
-							</tr>
-						</tbody>
-					</table>
-				</form>
-			</div>
-		</div>
-	</div>
-
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-
-</html>
+					foreach ($list as $item) {
+						echo '<tr class="row">';
+						echo '<td class="col-4">'.$item['item name'].'</td>';
+						echo '<td class="col-4">'.$item['price'].'</td>';
+						echo '<td class="col-4"> <input type="number" name="'.$item['input name'].'" max="10" min="0" maxLength="3" class="form-control"> </td>';
+						echo '</tr>';
+					}
+				?>
+				<tr class="row">
+					<td class="col-8">How did you find Bob's</td>
+					<td class="col-4">
+						<select name="find" class="custom-select">
+							<option value="regular">I'm a regular customer</option>
+							<option value="tv">TV advertising</option>
+							<option value="phone">Phone Directory</option>
+							<option value="mouth">Word of mouth</option>
+						</select>
+					</td>
+				</tr>
+				<tr class="row">
+					<td colspan="2" class="col-12"> <button type="submit" class="btn btn-primary float-right" > SUBMIT </button> </td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+<?php
+	require_once('view/footer.php');
+?>
--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -1,86 +1,68 @@
-<html>
+<?php
+	require_once('view/header.php');
+?>
+	<h1 class="card-title"> Bob's Auto Parts </h1>
+	<br>
+	<h3 class="card-title"> Order Results </h3>
 
-<head>
-	<title> Process Order </title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
+	<?php
+	define('TIRE_PRICE', 100);
+	define('OIL_PRICE', 100);
+	define('SPARK_PRICE', 100);
 
-<body>
+          echo '<p>Order Processed at ';
+          echo date('H:i, jS F Y');
+	echo '</p>';
 
-	<div class="container">
-		<div class ="card">
-			<div class="card-body">
-				<h1 class="card-title"> Bob's Auto Parts </h1>
-				<br>
-				<h3 class="card-title"> Order Results </h3>
+	//comment in php
+	/**Multiline comment
+	ad **/
+	$tireQty = $_POST['tireQty'];
+	$oilQty = $_POST['oilQty'];
+	$sparkQty = $_POST['sparkQty'];
+	$find = $_POST['find'];
 
-				<?php
-				define('TIRE_PRICE', 100);
-				define('OIL_PRICE', 100);
-				define('SPARK_PRICE', 100);
+	switch($find) {
+		case 'regular':
+			echo 'Regular Customer';
+			break;
+		case 'tv':
+			echo 'From TV Advertising';
+			break;
+		case 'phone':
+			echo 'From Phone Directory';
+			break;
+		case 'mouth':
+			echo 'From Word of Mouth';
+			break;
+		default:
+			echo 'Unknown Customer';
+			break;
+	}
 
-                echo '<p>Order Processed at ';
-                echo date('H:i, jS F Y');
-				echo '</p>';
+	echo '<p>Your order is as folows</p>';
+	echo $tireQty.' tires.<br/>';
+	//concatination in php is dot
+	echo "$oilQty bottles of oil.<br/>";
+	echo "$sparkQty spark plugs.<br/></br>";
 
-				//comment in php
-				/**Multiline comment
-				ad **/
-				$tireQty = $_POST['tireQty'];
-				$oilQty = $_POST['oilQty'];
-				$sparkQty = $_POST['sparkQty'];
-				$find = $_POST['find'];
+	$total = (TIRE_PRICE * $tireQty) + (OIL_PRICE * $oilQty) + (SPARK_PRICE * $sparkQty);
+	$vatable = $total / 1.12;
+	$vat = $total - $vatable;
 
-				switch($find) {
-					case 'regular':
-						echo 'Regular Customer';
-						break;
-					case 'tv':
-						echo 'From TV Advertising';
-						break;
-					case 'phone':
-						echo 'From Phone Directory';
-						break;
-					case 'mouth':
-						echo 'From Word of Mouth';
-						break;
-					default:
-						echo 'Unknown Customer';
-						break;
-				}
+	echo "VATable Amount: $vatable <br/>";
+	echo "VAT Amount (12%): $vat <br/>";
+	echo "Total Amount: $total <br/>";
 
-				echo '<p>Your order is as folows</p>';
-				echo $tireQty.' tires.<br/>';
-				//concatination in php is dot
-				echo "$oilQty bottles of oil.<br/>";
-				echo "$sparkQty spark plugs.<br/></br>";
+	echo "<br>Amount exceeded 500 but less than 1000? ".(500 < $total ? ($total < 1000 ? 'yes': 'no') : 'no').'<br><br>';
 
-				$total = (TIRE_PRICE * $tireQty) + (OIL_PRICE * $oilQty) + (SPARK_PRICE * $sparkQty);
-				$vatable = $total / 1.12;
-				$vat = $total - $vatable;
+	?>
 
-				echo "VATable Amount: $vatable <br/>";
-				echo "VAT Amount (12%): $vat <br/>";
-				echo "Total Amount: $total <br/>";
+	<br>
+	<br>
 
-				echo "<br>Amount exceeded 500 but less than 1000? ".(500 < $total ? ($total < 1000 ? 'yes': 'no') : 'no').'<br><br>';
+	<a href ="order-form.php" class="btn btn-primary"> Go Back </a>
 
-				?>
-
-				<br>
-				<br>
-
-				<a href ="order-form.php" class="btn btn-primary"> Go Back </a>
-
-			</div>
-		</div>
-	</div>
-
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php
+	require_once('view/footer.php');
+?>
