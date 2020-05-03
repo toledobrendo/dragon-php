d2b66e76857c799d975181dd42530e47b6cbaefb
CharAzu <markchristianyapan@yahoo.com>
2020-05-04 00:10:04 +0800

    Fixed errors in order-form, product, and order php

--- bobs-auto-parts/model/order.php
+++ bobs-auto-parts/model/order.php
@@ -2,26 +2,26 @@
   require_once('product.php');
 
   class Order {
-    private Product $products;
+    private $products;
     //include this if customer name is needed
     //private $customerName;
 
 
     public function __construct() {
       //Initialize the items in Bob's auto parts here
-
+      $this->products = array();
       //temporary list of products and prices
       $products_prices = [];
-      array_push($products_prices, 'Tires'=>100);
-      array_push($products_prices, 'Oil'=>20);
-      array_push($products_prices, 'Spark Plugs'=>50);
+      $products_prices['Tires'] = 100;
+      $products_prices['Oil'] = 20;
+      $products_prices['Spark Plugs'] = 50;
       //add more products here
 
       foreach ($products_prices as $name => $price) {
-        Product $product;
-        $product->setProduct(trim(preg_replace('/\s+/', ' ', $name), $name, $price);
+        $product = new Product();
+        $product->setProduct(trim(preg_replace('/\s+/', ' ', $name)), $name, $price);
         //pushes the object product to the array
-        array_push($products, $product);
+        array_push($this->products, $product);
       }
     }
 
@@ -31,7 +31,7 @@
 
     public function setQuantity($qtyArray) {
       $ite = 0;
-      for($qtyArray as $productID => $quantity) {
+      foreach($qtyArray as $productID => $quantity) {
         foreach ($products as $item) {
           if($item->productID == $productID) {
             $item->quantity = $quantity;
@@ -39,6 +39,10 @@
           }
         }
       }
+    }
+
+    public function __get($fieldName) {
+      return $this->$fieldName;
     }
   }
  ?>
--- bobs-auto-parts/model/product.php
+++ bobs-auto-parts/model/product.php
@@ -12,6 +12,10 @@
       $quantity = 0;
     }
 
+    public function __get($fieldName) {
+      return $this->$fieldName;
+    }
+
     public function setProduct($id, $name, $price) {
       $this->productID = $id;
       $this->productName = $name;
