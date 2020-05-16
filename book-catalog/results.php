e5049215f698e60ca79dd8779612fd03a0a4a21e
toledobrendo <toledo.brendo@gmail.com>
2020-05-16 17:16:18 +0800

    added mysql select code

--- book-catalog/results.php
+++ book-catalog/results.php
@@ -4,6 +4,12 @@
 </div>
 <div class="card-body">
   <?php
+    define('FIELDS', array(
+      'author' => 'author.name',
+      'title' => 'book.title',
+      'isbn' => 'book.isbn'
+    ));
+
     $searchType = $_POST['searchType'];
     $searchTerm = $_POST['searchTerm'];
 
@@ -17,14 +23,46 @@
 
       $dbError = mysqli_connect_errno();
       if ($dbError) {
-        throw new Exception('Error: Could not connect to database. Please try again later. '.$dbError, 1);
+        throw new Exception('Error: Could not connect to database. '.
+          'Please try again later. '.$dbError, 1);
       }
 
+      $query = 'SELECT author.name as author_name, book.title, book.isbn
+        FROM book
+        INNER JOIN author
+            ON author.id = book.author_id
+        WHERE '.FIELDS[$searchType].' LIKE \'%'.$searchTerm.'%\';';
+
+      echo $query.'<br/>';
+
+      $result = $db->query($query);
+
+      $resultCount = $result->num_rows;
+
+      echo '<p>Result for '.$searchType.' : '.$searchTerm.'</br>';
+      echo 'Number of books found: '.$resultCount;
+
+      echo '<div class="row">';
+      for ($ctr = 0; $ctr < $resultCount; $ctr++) {
+        $row = $result -> fetch_assoc();
+      ?>
+        <div class="card col-4 mx-1">
+          <div class="card-body">
+            <h6><?php echo $row['title'];?></h6>
+            <p>
+              By: <?php echo  $row['author_name'];?> <br/>
+              <?php echo $row['isbn']?>
+            </p>
+          </div>
+        </div>
+      <?php
+      }
+      echo '</div>';
     } catch (Exception $e) {
       echo $e->getMessage();
-      echo '<br/>';
-      echo '<a class="btn btn-secondary my-3" href="index.php">Go Back</a>';
     }
   ?>
+  <br/>
+  <a class="btn btn-secondary" href="index.php">Go Back</a>
 </div>
 <?php require_once('view-comp/footer.php');?>
