7ae6905c2f863dd10ad19b7f7f9fbd0fa1766c95
unknown <vera.david1214@gmail.com>
2020-05-16 15:58:10 +0800

    db sql

--- book-catalog/sql/db.sql
+++ book-catalog/sql/db.sql
@@ -2,3 +2,26 @@ CREATE DATABASE php_lesson_db;
 
 -- used for selecting db
 USE php_lesson_db;  
+
+CREATE TABLE IF NOT EXISTS author (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS book (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(255),
+	isbn VARCHAR(255),
+	author_id INT(6) UNSIGNED,
+	FOREIGN KEY (author_id) REFERENCES author(id)
+);
+
+INSERT INTO author(name) 
+VALUES 
+	('Michael Morgan'), 
+	('George RR Martin');
+
+INSERT INTO book (title, isbn, author_id)
+VALUES
+	('Java for Professional Developers', '0-672-316123-8', 1),
+	('A Game of Thrones', '1-141-5143123-5', 2);
