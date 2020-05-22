f9cdd784cea890e6c398189c028b64ae7702cc8c
CharAzu <markchristianyapan@yahoo.com>
2020-05-22 17:43:56 +0800

    Added files for book-catalog

---
+++ book-catalog/sql/db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE php_lesson_db;
+
+USE php_lesson_db;
+
+CREATE TABLE IF NOT EXISTS author (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS book (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  isbn VARCHAR(255),
+  author_id INT(6) UNSIGNED,
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  pic_url VARCHAR(255)
+);
+
+INSERT INTO author (name)
+VALUES
+  ('George RR Martin'),
+  ('Tomohito Oda'),
+  ('Jitakukeibihei');
+
+INSERT INTO book (title, isbn, author_id, pic_url)
+VALUES
+  ('A Game of Thrones', '1-141-5143123-5', 1, '../image/game-of-thrones.jpg'),
+  ('Komi Can\'t Communicate', '1-9747-0712-6', 2, '../image/komi-san.jpg'),
+  ('KonoSuba: God\'s Blessing on this Wonderful World!', '0-31-655337-7', 3, '../image/konosuba.jpg');
--- index.php
+++ index.php
@@ -30,6 +30,9 @@
 						<li class="list-group-item">
 							<a href="caesar-shift\caesar-shift.php"> Caesar Shift </a>
 						</li>
+						<li class="list-group-item">
+							<a href="caesar-shift\caesar-shift.php"> Book Catalog </a>
+						</li>
 					</ul>
 				</div>
 			</div>
