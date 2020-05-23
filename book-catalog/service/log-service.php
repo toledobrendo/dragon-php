052f026f503fed59125df8ebcb573cb8e0652442
thunderdaawn <jeanmichelc.gomez@gmail.com>
2020-05-23 17:41:28 +0800

    added latest codes

--- book-catalog/service/log-service.php
+++ book-catalog/service/log-service.php
@@ -2,7 +2,9 @@
 
 	function logMessage($message) {
 		$documentRoot = $_SERVER['DOCUMENT_ROOT'];
-		error_log(date('H:i, jS F Y').' : '.$message.PHP_EOL, 
+		$ipAddress = $_SERVER['REMOTE_ADDR'];
+
+		error_log(date('H:i, jS F Y').' - '.$ipAddress.' : '.$message.PHP_EOL, 
 			3, 
 			$documentRoot.'/../apache/logs/info.log'
 		); 
