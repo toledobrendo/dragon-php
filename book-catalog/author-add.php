aa36d8f4503f092659e40202d2dd85ad424946e4
unknown <vera.david1214@gmail.com>
2020-05-23 15:04:25 +0800

    fixed h2; added add author

--- book-catalog/author-add.php
+++ book-catalog/author-add.php
@@ -5,7 +5,17 @@
 <div class="card-header">Add Author</div>
 
 <div class="card-body">
-  
+  <form action="process-author-add.php" method="post">
+    <div class="form-group">
+      <label for="authorName">Author Name</label>
+      <input id="authorName" type="text" name="authorName" class="form-control">
+    </div>
+
+    <div>
+      <button class="btn btn-primary" type="submit">Submit</button>
+    </div>
+
+  </form>
 </div>
 
 <?php 
