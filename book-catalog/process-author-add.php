5cda9043e566c0f9159bc5e3e13ff8eaa73e2a8d
thunderdaawn <jeanmichelc.gomez@gmail.com>
2020-05-23 22:00:37 +0800

    added add-book & process-book-add.php

--- book-catalog/process-author-add.php
+++ book-catalog/process-author-add.php
@@ -10,49 +10,58 @@
 
 		$authorName = $_POST['authorName'];
 
-		try {
-
-			if(!$authorName) {
-				throw new Exception('Author details not complete. Please try again.');
-			}
-
-			@ $db = new mysqli('127.0.0.1:3306', 'student', 'mydev040100', 'php_lesson_db');
-
-			$dbError = mysqli_connect_errno();
-
-			if($dbError) {
-				throw new Exception('Error: Could not connect to database. Please try again later. '.$dbError, 1);
-			}
-
-			// Query by query string concatenation
-	        // $query = 'insert into author (name) values (\''.$authorName.'\')';
-	        // $result = $db->query($query);
-	        //
-	        // if ($result) {
-	        //   echo $db->affected_rows." author inserted into the database.";
-	        // } else {
-	        //   throw new Exception('Error: The author was not added.');
-	        // }
-
-	        // $authorName = $db->real_escape_string($authorName);
-
-		    // Query by prepared statements
-		    $query = 'insert into author (name) values (?)';
-		    $stmt = $db->prepare($query);
-		    $stmt->bind_param("s", $authorName);
-		    $stmt->execute();
-
-		    $affectedRows = $stmt->affected_rows;
-		    if ($affectedRows > 0) {
-		    	echo $affectedRows." author inserted into the database.";
-		   	} else {
-		    	throw new Exception('Error: The author was not added.');
+		try{
+		    
+		    if (!$authorName) {
+		    	throw new Exception('Author details not complete. Please try again.');
 		    }
 
-		      $stmt->close();
-								
+		    @ $db = new mysqli('127.0.0.1:3306', 'student', 'mydev040100', 'php_lesson_db');
+
+		    $dbError = mysqli_connect_errno();
+		    if ($dbError) {
+		    	throw new Exception('Error: Could not connect to database. Please try again later.');
+		    }
+
+	      	// Query by query string concatenation
+	      	// $query = 'insert into author (name) values (\''.$authorName.'\')';
+	      	// $result = $db->query($query);
+	      	//
+	      	// if ($result) {
+	      	//   echo $db->affected_rows." author inserted into the database.";
+	      	// } else {
+	      	//   throw new Exception('Error: The author was not added.');
+	      	// }
+
+		    // $authorName = $db->real_escape_string($authorName);
+
+		    // check if author already exists in the db
+		    $getAuthorIdQuery = 'SELECT id FROM author WHERE name = "'.$authorName.'"';
+		    $result = $db->query($getAuthorIdQuery);
+		    $resultCount = $result->num_rows;
+
+		    if($resultCount == 0) {
+
+		    	// Query by prepared statements
+			    $query = 'insert into author (name) values (?)';
+			    $stmt = $db->prepare($query);
+			    $stmt->bind_param("s", $authorName);
+			    $stmt->execute();
+
+			   	$affectedRows = $stmt->affected_rows;
+			    if ($affectedRows > 0) {
+			      echo $affectedRows." author successfully inserted into the database.";
+			    } else {
+			    	throw new Exception('Error: The author was not added.');
+			    }
+
+			    $stmt->close();
+		    } else {
+				throw new Exception('Error: This author already exists in the database.');
+		    }
+							
 		} catch (Exception $e) {
-			error_log(e->getMessage());
+			error_log($e->getMessage());
 			echo $e->getMessage();
 		}
 
--- book-catalog/process-book-add.php
+++ book-catalog/process-book-add.php
@@ -27,27 +27,63 @@
 				throw new Exception('Error: Could not connect to database. Please try again later. '.$dbError, 1);
 			}
 
+			// check if author already exists in the db
+		    $getAuthorIdQuery = 'SELECT id FROM author WHERE name = "'.$authorName.'"';
+		    $result = $db->query($getAuthorIdQuery);
+		    $resultCount = $result->num_rows;
 
-		    $id_query = 'SELECT * FROM author WHERE name = '.$authorName;
-			$result = $db->query($id_query);
+		    // author does not exist in rhe db
+		    if($resultCount == 0) {
+
+		    	// add new author into the author table
+			    $addAuthorQuery = 'INSERT INTO author (name) values (?)';
+			    $authorstmt = $db->prepare($addAuthorQuery);
+			    $authorstmt->bind_param("s", $authorName);
+			    $authorstmt->execute();
+
+			    // echo the result to the user
+			    $authorAffectedRows = $authorstmt->affected_rows;
+			    if ($authorAffectedRows < 0) {
+			      throw new Exception('Error: The author was not added.');
+			    }
+
+			    $authorstmt->close();
+
+			    // get the id of the author that was newly inserted
+		    	$getAuthorIdQuery = 'SELECT id FROM author WHERE name = "'.$authorName.'"';
+		    	$result = $db->query($getAuthorIdQuery);
+			}
+
 			$row = $result->fetch_assoc();
 
-			$query = 'INSERT INTO book (title, author_id, isbn, image_src) values (?, ?, ?, ?)';
+			// check if book already exists in the db
+		    $getBookIdQuery = 'SELECT id FROM book WHERE author_id = "'.$row['id'].'" AND title = "'.$bookTitle.'" AND isbn = "'.$isbn.'"';
+		    $result = $db->query($getBookIdQuery);
+		    $resultCount = $result->num_rows;
 
-		    $stmt = $db->prepare($query);
-		    $stmt->bind_param("ssss", $bookTitle, $row['id'], $isbn, $imageURL);
-		    $stmt->execute();
+		    if($resultCount == 0) {
 
-		    $affectedRows = $stmt->affected_rows;
-		    if ($affectedRows > 0) {
-		    	echo $affectedRows." book inserted into the database.";
-		   	} else {
-		    	throw new Exception('Error: The book was not added.');
-		    }
-   
-		    $stmt->close();
-								
-		} catch (Exception $e) {
+				// add the new book onto the database
+				$addBookQuery = 'INSERT INTO book (title, author_id, isbn, image_src) values (?, ?, ?, ?)';
+				$bookstmt = $db->prepare($addBookQuery);
+				$bookstmt->bind_param("siss", $bookTitle, $row['id'], $isbn, $imageURL);
+				$bookstmt->execute();
+
+				// echo the result of this to the user
+				$bookAffectedRows = $bookstmt->affected_rows;
+				if ($bookAffectedRows > 0) {
+					echo $bookAffectedRows." book was successfully inserted into the database.";
+				} else {
+					 throw new Exception('Error: The book was not added.');
+				}
+
+				$bookstmt->close();
+
+			} else {
+				throw new Exception('Unable to add book. The book you entered is already in the database.');
+			}
+
+		}catch (Exception $e) {
 			echo $e->getMessage();
 		}
 
@@ -56,7 +92,7 @@
 </div>
 
 <div class="card-footer">
-	<a class="btn btn-secondary" href="author-add.php">Back</a>
+	<a class="btn btn-secondary" href="book-add.php">Back</a>
 </div>
 
 </body>
