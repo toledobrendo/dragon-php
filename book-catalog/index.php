0dd2f0f950fd9ac8dd5fc259ae187c7685fe8dc6
Paul Vincent Salvador Garcia Magbojos <paul.vincent.magbojos@gmail.com>
2020-06-20 14:31:52 +0800

    Implemented showing search results in results.php

--- book-catalog/index.php
+++ book-catalog/index.php
@@ -1,24 +1,24 @@
 <?php require_once './view-comp/header.php' ?>
-        <div class="card-header">
-          Search Book
-        </div>
-        <div class="card-body">
-          <form action="results.php" method="post">
-            <div class="form-group">
-              <label for="searchType">Choose Search Type</label>
-              <select class="form-control" id="searchType" name="searchType">
-                <option value="author">Author</option>
-                <option value="title">Title</option>
-                <option value="isbn">ISBN</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="searchTerm">Search Type</label>
-              <input class="form-control" type="text" id="searchTerm" name="searchTerm">
-            </div>
-            <div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-          </form>
-        </div>
+<div class="card-header">
+  Search Book
+</div>
+<div class="card-body">
+  <form action="results.php" method="post">
+    <div class="form-group">
+      <label for="searchType">Choose Search Type</label>
+      <select class="form-control" id="searchType" name="searchType">
+        <option value="author">Author</option>
+        <option value="title">Title</option>
+        <option value="isbn">ISBN</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="searchTerm">Search Type</label>
+      <input class="form-control" type="text" id="searchTerm" name="searchTerm">
+    </div>
+    <div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</div>
 <?php require_once './view-comp/footer.php'; ?>
--- book-catalog/results.php
+++ book-catalog/results.php
@@ -4,16 +4,53 @@ require_once './view-comp/header.php';
 $searchType = $_POST['searchType'];
 $searchTerm = $_POST['searchTerm'];
 
-if(!$searchType || !$searchTerm) {
-  echo 'You have not entered search details. Please go back and try again';
-} else {
+define('FIELDS', array(
+  'author' => 'author.name',
+  'title' => 'book.title',
+  'isbn' => 'book.isbn'));
 ?>
-        <div class="card-body">
-          <?php
-            echo 'Hello World';
-          ?>
-        </div>
-<?php
-}
+<div class="card-header">
+  Search Results
+</div>
+<div class="card-body">
+  <?php
+  if(!$searchType || !$searchTerm) {
+    echo 'You have not entered search details. Please go back and try again';
+  } else {
+    @ $db = new mysqli('127.0.0.1:3306','student','cndpgmfNhbsXbild','php_lesson_db');
 
-require_once './view-comp/footer.php'; ?>
+    $dbError = mysqli_connect_errno();
+
+    if($dbError) {
+      echo 'Error: Could not connect to database. Please try again later.'.$dbError;
+    } else {
+      $query = "SELECT author.name AS author_name, book.title, book.isbn
+      FROM book INNER JOIN author ON author.id = book.author_id
+      WHERE ".FIELDS[$searchType]." LIKE '%".$searchTerm."%';";
+
+      $result = $db->query($query);
+
+      $resultCount = $result->num_rows;
+
+      echo "<p>Result for ".$searchType." : ".$searchTerm;
+      echo "<p>Number of books found: ".$resultCount;
+
+      for ($ctr = 0; $ctr < $resultCount; $ctr++) {
+            $row = $result->fetch_assoc();
+    ?>
+        <div class="card col-4">
+          <div class="card-body">
+              <h6><?php echo $row['title']; ?></h6>
+              <p>
+                By: <?php echo $row['author_name']; ?><br/>
+                <?php echo $row['isbn']; ?>
+              </p>
+          </div>
+        </div>
+    <?php
+      }
+    }
+  }
+  ?>
+</div>
+<?php require_once './view-comp/footer.php'; ?>
