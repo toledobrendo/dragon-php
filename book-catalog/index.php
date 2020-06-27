0bfa9d07afd393745fa84ae1c23c420498643a8a
chris4b7 <201801139@iacademy.ph>
2020-06-27 21:55:29 +0800

    index new codes refresh atmpt4

--- book-catalog/index.php
+++ book-catalog/index.php
@@ -1,51 +1,25 @@
-<?php
-  function isActive($page){
-    return strpos($_SERVER['REQUEST_URI'], $page);
-
-}
-
-?>
-
-
-<html>
-<head>
-  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-
-<body>
-  <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div href="index.php" class = "navbar-brand">Book Catalog</div>
-
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-
-          <li class="nav-item <?php if(isActive('index.php')) echo 'active'; ?>">
-            <a class="nav-link" href="index.php">Book Search</a>
-          </li>
-
-
-        </ul>
-      </div>
-  </div>
-  <!-- hello-world.php or hello_world.php -->
-  <div class="container">
-        <div class="card">
-          <div class="card-body">
-            <?php ?>
-          </div>
-        </div>
-  </div>
-
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-</body>
-
-
-</html>
-    
+<?php require_once('view-comp/header.php'); ?>
+<div class="card-header">
+  Search Book
+</div>
+<div class="card-body">
+  <form action="results.php" method="post">
+    <div class="form-group">
+      <label for="searchType">Choose Search Type</label>
+      <select class="form-control" id="searchType" name="searchType">
+        <option value="author">Author</option>
+        <option value="title">Title</option>
+        <option value="isbn">ISBN</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="searchTerm">Search Term</label>
+      <input type="text" id="searchTerm" name="searchTerm"
+        class="form-control" placeholder="Search Term"/>
+    </div>
+    <div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</div>
+<?php require_once('view-comp/footer.php'); ?>
