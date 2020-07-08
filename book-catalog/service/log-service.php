1b8840789467e28062a15d5a74b87c8d0280efe9
chris4b7 <201801139@iacademy.ph>
2020-07-08 16:14:00 +0800

    for HW Log-service

--- book-catalog/service/log-service.php
+++ book-catalog/service/log-service.php
@@ -1,14 +1,13 @@
 <?php
 
   function logMessage($message) {
-  	
+
     $documentRoot = $_SERVER['DOCUMENT_ROOT'];
     $ipAddress = $_SERVER['REMOTE_ADDR'];
-    error_log(date('H:i, jS F Y').' - '.$ipAddress.' : '.$message.PHP_EOL,
-      3,
+
+    error_log(date('H:i, jS F Y').' - '.$ipAddress.' : '.$message.PHP_EOL,3,
       $documentRoot.'/../apache/logs/info.log'
     );
 
   }
-
 ?>
