70b692dbbfef9124271bac31606dd150cf501233
chris4b7 <201801139@iacademy.ph>
2020-07-08 16:16:23 +0800

    author fix

--- book-catalog/author-add.php
+++ book-catalog/author-add.php
@@ -1,23 +1,34 @@
-<?php require_once('view-comp/header.php');?>
+<?php 
+	require_once('view-comp/header.php');
+?>
+
 <div class="card-header">
  Add author
 </div>
 
 <div class="card-body">
-  <form action="process-author-add.php" method="post">
+	
+	  <form action="process-author-add.php" method="post">
 
-    <div class="form-group">
+		    <div class="form-group">
 
-      <label for="authorName">Author name</label>
-      <input id="authorName" name="authorName" class="form-control" type="text"/>
+			      <label for="authorName">Author name</label>
+			      <input id="authorName" name="authorName" class="form-control" type="text"/>
 
-    </div>
+		    </div>
 
-      <div>
-        <button class="btn btn-primary" type="submit">Submit</button>
-      </div>
-      
-  </form>
+
+		    <div>
+		       <button class="btn btn-primary" type="submit">Submit</button>
+		    </div>
+
+
+	      
+	  </form>
 
  </div>
- <?php require_once('view-comp/footer.php') ?>
+
+
+ <?php 
+ 	require_once('view-comp/footer.php');
+ ?>
