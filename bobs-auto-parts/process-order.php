c44f81e9624ed6975fa2c3e10920c59a4d6f6091
chris4b7 <201801139@iacademy.ph>
2020-07-08 10:02:44 +0800

    added for HW4 and modifications

--- bobs-auto-parts/process-order.php
+++ bobs-auto-parts/process-order.php
@@ -1,23 +1,16 @@
 <?php
+  require_once('view/header.php');
+  require_once('model/content.php');
+  require_once('model/item-inv.php');
   require_once('service/order-service.php');
+  require_once('exception/file-not-found-exception.php');
 
-  define('TIRE_PRICE', 100);
-  define('OIL_PRICE',50);
-  define('SPARK_PRICE',30);
+  // define('TIRE_PRICE', 100);
+  // define('OIL_PRICE',50);
+  // define('SPARK_PRICE',30);
 
 ?>
 
-
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
           <h3 class="card-title">Order Result</h3>
           <?php
 
@@ -34,7 +27,7 @@
 
            $oilQty = $_POST['oilQty'] ? $_POST['oilQty'] :0;
 
-           $sparkeQty= $_POST['sparkeQty'] ? $_POST['sparkeQty'] :0;
+           $sparkQty= $_POST['sparkQty'] ? $_POST['sparkQty'] :0;
            $find = $_POST ['find'];
 
            switch($find){
@@ -84,7 +77,7 @@
 
 
 
-           $totalQty = @($tireQty + $oilQty + $sparkeQty);
+           $totalQty = @($tireQty + $oilQty + $sparkQty);
 
            if ($totalQty == 0){
             echo 'You didnt order anything. <br/><br/>';
@@ -97,8 +90,8 @@
             if($oilQty > 0)
              echo $oilQty.' oils <br/>';
 
-            if($sparkeQty > 0)
-             echo $sparkeQty.' sparks <br/><br/>';
+            if($sparkQty > 0)
+             echo $sparkQty.' sparks <br/><br/>';
 
             echo '<br/>';
            }
@@ -109,7 +102,7 @@
 
            $tireAmount = @($tireQty * TIRE_PRICE);
            $oilAmount = @($oilQty * OIL_PRICE);
-           $sparkAmount = @($sparkeQty * SPARK_PRICE);
+           $sparkAmount = @($sparkQty * SPARK_PRICE);
 
            $totalAmount = (float) $tireAmount;
 
@@ -158,17 +151,16 @@
            echo 'is $totalAmountTwo empty? ' .(empty($totalAmountTwo)?'Yes' : 'No').'<br/>';
 
 
-           saveOrder($tireQty, $oilQty, $sparkeQty, $totalAmount);
+           saveOrder($tireQty, $oilQty, $sparkQty, $totalAmount);
 
 
            ?>
+       </div>
+
+        <div class="card-footer">
+            <a class="btn btn-info" href="order-form.php">Go Back</a>
+
         </div>
-      </div>
-    </div>
 
-     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
 
-</html>
+<?php require_once('view/footer.php') ?>
