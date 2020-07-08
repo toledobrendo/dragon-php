e8a0c38af7d7890f652d18751830937205766b18
chris4b7 <201801139@iacademy.ph>
2020-07-08 10:01:32 +0800

    added for HW3 and activities

--- bobs-auto-parts/order-form.php
+++ bobs-auto-parts/order-form.php
@@ -1,13 +1,10 @@
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
+<?php
+
+  require_once('view/header.php');
+  require_once('model/content.php');
+  require_once('model/item-inv.php');
+
+?>
           <h3 class="card-title">Orderform</h3>
           <form  action="process-order.php" method="post">
             <table class = "table">
@@ -25,26 +22,26 @@
                 <?php
                   $products = array(array('desc' => 'Tires', 'name' => 'tireQty','price' => 100 ),
                                     array('desc' => 'Oil', 'name' => 'oilQty','price' => 200 ),
-                                    array('desc' => 'Spark Plugs', 'name' => 'sparkeQty','price' => 300 ));
+                                    array('desc' => 'Spark Plugs', 'name' => 'sparkQty','price' => 300 ));
 
 
-                  foreach ($products as $product) {
-                      echo '<tr class="row">
-                        <td class="col-5"> '.$product['desc'].'</td>
-                        <td class="col-2"> '.$product['price'].'</td>
-                        <td class="col-4">
-                            <input type="text" name=""'.$product['name'].'" maxlength="3" class="form-control"/>
-                        </td>
+                  foreach($inventory as $products){
+                    echo '<tr class="row">
 
+                            <td class="col-4">'.$products->prodName.'
+                            </td>
 
-                      </tr>';
+                            <td class="col-4">'.$products->price.'
+                            </td>
+
+                            <td class="col-4">
+                              <input type="number" name="'.$products->misc.'"  min="0" class="form-control"/>
+                            </td>
+
+                          </tr>';
                   }
 
                 ?>
-
-
-
-
 
 
 
@@ -94,20 +91,15 @@
                 <tr class="row">
                   <td colspan="2" class="col-9">
                     <a href="freight-cost.php" class="btn btn-secondary float-right">Freight Cost</a>
-                    <button type="submite" class="btn btn-primary float-right">Submit</button>
+                    <button type="submit" class="btn btn-primary float-right"> Submit </button>
                   </td>
 
                 </tr>
               </tbody>
             </table>
           </form>
-        </div>
-      </div>
-    </div>
 
-     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
 
-</html>
+<?php
+  require_once('view/footer.php');
+?>
