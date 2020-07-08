1859f5987d38ec9e30c4ff563baa115b83d9378d
chris4b7 <201801139@iacademy.ph>
2020-07-08 16:19:23 +0800

    still has bug , site cant redirect to result.php as of now

--- book-catalog/result.php
+++ book-catalog/result.php
@@ -13,7 +13,8 @@
     define('FIELDS', array(
       'author' => 'author.name',
       'title' => 'book.title',
-      'isbn' => 'book.isbn'
+      'isbn' => 'book.isbn',
+      'image' => 'book.imgSrc'
     ));
 
     $searchType = $_POST['searchType'];
@@ -26,7 +27,7 @@
             }
 
             // 127.0.0.1 = localhost
-            @ $db = new mysqli('127.0.0.1:3306', 'student', '123qwe', 'php_lesson_db');
+             @ $db = new mysqli('localhost', 'root', '', 'php_lesson_db');
 
             $dbError = mysqli_connect_errno();
             if ($dbError) {
@@ -37,46 +38,49 @@
             // save to db: 'http://localhost/dragon-php/book-catalog/image/manila.jpg'
             // save to db: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1562726234l/13496.jpg'
 
-            $query = 'SELECT author.name as author_name, book.title, book.isbn
-              FROM book
-              INNER JOIN author
-                  ON author.id = book.author_id
-              WHERE '.FIELDS[$searchType].' LIKE \'%'.$searchTerm.'%\';';
+            $query = 'SELECT author.name as author_name, book.title, book.isbn, book.imgSrc
+            FROM book
+            INNER JOIN author
+                ON author.id = book.author_id
+            WHERE '.FIELDS[$searchType].' LIKE \'%'.$searchTerm.'%\';';
 
-            logMessage($query);
+          logMessage($query);
 
-            $result = $db->query($query);
+          $result = $db->query($query);
 
-            $resultCount = $result->num_rows;
+          $resultCount = $result->num_rows;
 
-            echo '<p>Result for '.$searchType.' : '.$searchTerm.'</br>';
-            echo 'Number of books found: '.$resultCount;
+          echo '<p>Result for '.$searchType.' : '.$searchTerm.'</br>';
+          echo 'Number of books found: '.$resultCount;
 
-            echo '<div class="row">';
-            for ($ctr = 0; $ctr < $resultCount; $ctr++) {
-              $row = $result -> fetch_assoc();
-            ?>
-              <div class="card col-4 mx-1">
-                <div class="card-body">
-                  <h6><?php echo $row['title'];?></h6>
-                  <p>
-                    By: <?php echo  $row['author_name'];?> <br/>
-                    <?php echo $row['isbn']?>
-                  </p>
-                </div>
+          echo '<div class="row">';
+          for ($ctr = 0; $ctr < $resultCount; $ctr++) {
+            $row = $result -> fetch_assoc();
+          ?>
+            <div class="card col-4 text-center">
+              <div class="card-body">
+                <img src="<?php echo $row['imgSrc']; ?>" alt="photo" height="320px" width="240px">
+                <h6><?php echo $row['title'];?></h6>
+                <p>
+                  By: <?php echo  $row['author_name'];?> <br/>
+                  <?php echo $row['isbn']?>
+                </p>
               </div>
-            <?php
-            }
-            echo '</div>';
+            </div>
 
-    } catch (Exception $e) {
-      echo $e->getMessage();
-    }
-  ?>
+          <?php
+                }
+                echo '</div>';
+              } catch (Exception $e) {
+                echo $e->getMessage();
+              }
+          ?>
 
-  <br/>
+      <br/>
+      <a class="btn btn-secondary" href="index.php">Go Back</a>
+    </div>
+    
 
-  <a class="btn btn-secondary" href="index.php">Go Back</a>
-</div>
-
-<?php require_once('view-comp/footer.php');?>
+<?php
+require_once('view-comp/footer.php');
+?>
