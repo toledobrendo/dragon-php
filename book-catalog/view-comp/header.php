563b3d6f37d702b336d3c5ffe7ab24a48386661f
chris4b7 <201801139@iacademy.ph>
2020-07-08 16:15:34 +0800

    header and footer fix

--- book-catalog/view-comp/header.php
+++ book-catalog/view-comp/header.php
@@ -1,29 +1,29 @@
-<?php
+<?php 
+
   function isActive($page) {
     return strpos($_SERVER['REQUEST_URI'], $page);
   }
+
 ?>
+
 <html>
 
-  <head>
+<head>
+  <title> Book Catalog </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" 
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+          crossorigin="anonymous">
+        
+</head>
 
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossorigin="anonymous">
+<body>
 
-  </head>
+  <div class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-
-
-  <body>
-
-
-    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div href="index.php" class="navbar-brand">Book Catalog</div>
-      <div class="collapse navbar-collapse">
+    <div href="index.php" class="navbar-brand"> Book Catalog </div>
         <ul class="navbar-nav">
           <li class="nav-item <?php if(isActive('index.php')) echo 'active'; ?>">
             <a class="nav-link" href="index.php">Book Search</a>
@@ -31,8 +31,10 @@
           <li class="nav-item <?php if(isActive('author-add.php')) echo 'active'; ?>">
             <a class="nav-link" href="author-add.php">Add Author</a>
           </li>
-        </ul>
-      </div>
-    </div>
-    <div class="container">
-      <div class="card my-3">
+          <li class="nav-item <?php if(isActive('book-add.php')) echo 'active'; ?>">
+            <a class="nav-link" href="book-add.php">Add Book</a>
+          </li>
+  </div>
+
+  <div class="container">
+    <div class ="card my-3">
