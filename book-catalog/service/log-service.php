9745efe183f29b45bc9f36b2fc37f54f5a1a2b27
chris4b7 <201801139@iacademy.ph>
2020-07-10 10:40:33 +0800

    fix attempt

--- book-catalog/service/log-service.php
+++ book-catalog/service/log-service.php
@@ -5,9 +5,7 @@
     $documentRoot = $_SERVER['DOCUMENT_ROOT'];
     $ipAddress = $_SERVER['REMOTE_ADDR'];
 
-    error_log(date('H:i, jS F Y').' - '.$ipAddress.' : '.$message.PHP_EOL,3,
-      $documentRoot.'/../apache/logs/info.log'
-    );
+    error_log(date('H:i, jS F Y').' - '.$ipAddress.' : '.$message.PHP_EOL,3,$documentRoot.'/../apache/logs/info.log' );
 
   }
 ?>
