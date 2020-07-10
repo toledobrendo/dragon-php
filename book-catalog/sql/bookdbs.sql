d54aaef5bd5c434b31aca13b4d1bb2fa7b186b8a
chris4b7 <201801139@iacademy.ph>
2020-07-11 01:33:28 +0800

    booksql fixed gg

--- book-catalog/sql/bookdbs.sql
+++ book-catalog/sql/bookdbs.sql
@@ -5,7 +5,8 @@ USE bookdbs;
 
 CREATE TABLE IF NOT EXISTS author (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255)
+  name VARCHAR(255),
+  CONSTRAINT un_author_name UNIQUE (name)
 );
 
 
@@ -14,8 +15,10 @@ CREATE TABLE IF NOT EXISTS book (
   title VARCHAR(255),
   isbn VARCHAR(255),
   author_id INT(6) UNSIGNED,
+  imgSrc VARCHAR(255),
   FOREIGN KEY (author_id) REFERENCES author(id)
 );
+
 
 
 
@@ -26,17 +29,21 @@ VALUES
   ('Kentaro Miura');
 
 
+-- V1
+-- INSERT INTO book (title, isbn, author_id) VALUES
+--   ('Java for Professional Developers', '0-672-316123-8', 1),
+--   ('A Game of Thrones', '1-141-5143123-5', 2),
+--   ('Berserk Vol 1', '3-9781593070205-1', 3);
 
-INSERT INTO book (title, isbn, author_id) VALUES
-  ('Java for Professional Developers', '0-672-316123-8', 1),
-  ('A Game of Thrones', '1-141-5143123-5', 2),
-  ('Berserk Vol 1', '3-9781593070205-1', 3);
+-- UPDATE book SET imgSrc = '/url.png' WHERE id = 1;
 
 
+-- V2
+INSERT INTO book (title, isbn, author_id, imgSrc) VALUES
+  ('Java for Professional Developers', '0-672-316123-8', 1, 'image/javaDev.jpg'),
+  ('A Game of Thrones', '1-141-5143123-5', 2, 'image/GoT.jpg'),
+  ('Berserk Vol 1', '3-9781593070205-1', 3,'image/berserk.jpg');
 
-  
+  -- UPDATE book SET imgSrc = '/url.png' WHERE id = 1;
 
-ALTER TABLE book ADD COLUMN pic_url VARCHAR(255);
-UPDATE book SET pic_url = '/url.png' WHERE id = 1;
 
-ALTER TABLE author ADD CONSTRAINT un_author_name UNIQUE (name);
